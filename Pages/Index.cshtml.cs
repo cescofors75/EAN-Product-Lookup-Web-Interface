060b721c61c78a5bb9f0eb308cc7237db21403aa
cesco <cesco@euro4x4parts.com>
2022-05-26 15:42:43 +0200

    debug

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -8,12 +8,7 @@ namespace webpageC_.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+   
     
     //[BindProperty]
     public Reference reference { get; set; }
